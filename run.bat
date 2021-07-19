323ec0f80ab4120b94c57d29f050e6f88bacf5d9
Vladislav Shiyanov <vladislav.shiyanov@mainconcept.com>
2021-07-20 00:05:48 +0700

    add new app and template

---
+++ run.bat
@@ -0,0 +1,16 @@
+rem django-admin startproject project_name
+
+call python manage.py runserver
+
+rem for make new application run command bellow and add app name to settings.py  INSTALLED_APPS section
+
+rem python manage.py startapp app_name
+rem also you can make urls.py in this app 
+rem  and also create folder 'templates' in this apps for render views in views.py and make folder with 'app_name' inside 'templates' folder
+
+
+rem https://www.bootstrapcdn.com/  for link css
+rem <link rel="stylesheet", href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+
+rem https://getbootstrap.com/
+rem https://getbootstrap.com/docs/5.0/examples/
